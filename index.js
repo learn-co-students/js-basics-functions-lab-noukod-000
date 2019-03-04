e05f139f0112a214d1aac3a046a97063267def5d
Saint-Jean Germila <germila@noukod.com>
2019-03-04 16:44:52 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ function distanceFromHqInBlocks (distance) {
 }
 
 function distanceFromHqInFeet (distance) {
-  return distanceFromHqInBlocks(distance) *264;
+  return distanceFromHqInBlocks(distance) *264
 }
 
 function distanceTravelledInFeet (uptown, downtown) {
   return Math.abs(downtown - uptown)  * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceTravelled = distanceTravelledInFeet(start, destination);
+  if (distanceTravelled < 400) {
+    return 0;
+  }  else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+          return 0.02 * (distanceTravelled - 400);
+  }
+  else if ((distanceTravelled > 2000 && distanceTravelled < 2500)) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
 }
