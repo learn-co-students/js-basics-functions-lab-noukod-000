8643436b78feed7315dbc144fbd48c2f53677fce
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-08-22 17:51:43 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ function distanceTravelledInFeet (start, end) {
   //returns the number of blocks given a value
   return (Math.abs(start - end) * 264);
 }
-
+ 
 function calculatesFarePrice (start, end) {
+  const distance = distanceTravelledInFeet(start, end)
   
+  if (distance < 400) {
+    return 0
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far';
+  }
+  //returns the number of blocks given a value
 }
+
+  
+ 
