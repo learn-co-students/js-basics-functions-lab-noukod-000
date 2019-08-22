8d25c74575646f70cb5908df6eb9a4b43ebf490a
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-08-22 17:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return block - 42
+  } else {
+    return 42 - block
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264
+}
+  
+function distanceTravelledInFeet (start, end) {
+  //returns the number of blocks given a value
+  return (Math.abs(start - end) * 264);
+}
+
+function calculatesFarePrice (start, end) {
+  
+}
